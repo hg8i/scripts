8983e418e3d15a6104f63030ae49bab157c66240
hg8i <aaronsw@umich.edu>
2019-05-22 15:33:46 +0200

    Script for reading hdf5's

---
+++ h5view.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2
+
+"""
+# Script for viewing the inards of hdf5's
+"""
+
+import h5py, sys, glob
+
+def red(*string):
+    """ return string as red """
+    string = [str(s) for s in string]
+    ret = "\033[31m{0}\033[39m".format(" ".join(string))
+    return ret
+
+def green(*string):
+    """ return string as green """
+    string = [str(s) for s in string]
+    ret = "\033[32m{0}\033[39m".format(" ".join(string))
+    return ret
+
+
+paths = sys.argv[1:]
+# print "Reading", iPattern
+
+# paths = glob.glob(iPattern)
+# print "Reading",paths
+for path in paths:
+    print "Showing",path
+    iFile = h5py.File(path,"r")
+    print "\tCat \t#Vars\t#Entries"
+    for cat in iFile.keys():
+        varLengths = set([len(iFile[cat][v]) for v in iFile[cat].keys()])
+        if len(varLengths)>1:
+            lenString = red("Length miss-match: {0}".format(varLengths))
+        elif len(varLengths)==1:
+            lenString = green(varLengths.pop())
+        else:
+            lenString = "None"
+        print "\t{0}\t{1}\t{2}".format(cat,len(iFile[cat]),lenString)
+            
+
+
