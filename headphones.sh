f6419030e311e250fb01e966586ac6c6bdfc8a01
hg8i <aaronsw@umich.edu>
2019-07-07 16:15:29 +0200

    Added pause for connect

--- headphones.sh
+++ headphones.sh
@@ -8,6 +8,7 @@ coproc bluetoothctl
 # echo -e 'info 28:11:A5:D9:84:B0\nexit' >&${COPROC[1]}
 echo -e 'scan on\n' >&${COPROC[1]}
 echo -e 'connect 28:11:A5:D9:84:B0\n' >&${COPROC[1]}
+sleep 3
 echo -e 'exit\n' >&${COPROC[1]}
 
 
