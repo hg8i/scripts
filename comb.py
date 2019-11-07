023cc010d61bc83ec74816af2d4c5529109c8589
hg8i <aaronsw@umich.edu>
2019-11-07 14:59:15 +0100

    add in quad

---
+++ comb.py
@@ -0,0 +1,10 @@
+#!/bin/env python2
+import sys,numpy,os
+
+# add values in quadrature, put in clip
+
+inputs = [float(i)**2 for i in sys.argv[1:]]
+comb = numpy.sqrt(numpy.sum(inputs))
+
+print "Added in quadrature",comb
+os.popen("echo '{0:.3f}' | xclip".format(comb))
